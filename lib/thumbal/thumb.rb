3e63011d7ec78bbe5c54f40a54db79e195706b1e
tali <atalyad@gmail.com>
2014-05-25 16:53:29 +0300

    thumb size

--- lib/thumbal/thumb.rb
+++ lib/thumbal/thumb.rb
@@ -5,7 +5,7 @@ class Thumb < ActiveRecord::Base
 
   belongs_to :thumbnail_experiment
   attr_accessible :image, :clicks, :impressions, :thumbnail_experiment_id
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", :path => "/system/static/thumbs/abtest/:id/:style_:basename.:extension"
+  has_attached_file :image, :styles => { :medium => "250x250>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png", :path => "/system/static/thumbs/abtest/:id/:style_:basename.:extension"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 
