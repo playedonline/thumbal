aec052c34647da42af67bc9b0bb1edcfd8acb75e
tali <atalyad@gmail.com>
2014-05-28 11:33:19 +0300

    make method an instance method

--- lib/thumbal/experiment.rb
+++ lib/thumbal/experiment.rb
@@ -266,7 +266,7 @@ module Thumbal
       0
     end
 
-    def get_sorted_alternatives
+    def self.get_sorted_alternatives
       return [] if alternatives.nil?
       alternatives.sort_by {|alt| get_alternative_participants(alt.name) != 0 ? get_alternative_clicks(alt.name) / get_alternative_participants(alt.name) : 0}.reverse
     end
